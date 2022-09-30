79851bb4d55fff9b2fb77275f98172e9e4e1a1d2
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2022-09-30 23:24:01 +0700

    feat: DP-02: create a new display element

--- 02-observer/src/main/java/org/example/observerpattern/WeatherStation.java
+++ 02-observer/src/main/java/org/example/observerpattern/WeatherStation.java
@@ -2,6 +2,7 @@ package org.example.observerpattern;
 
 import org.example.observerpattern.observer.CurrentConditionsDisplay;
 import org.example.observerpattern.observer.ForecastDisplay;
+import org.example.observerpattern.observer.HeatIndexDisplay;
 import org.example.observerpattern.observer.StatisticsDisplay;
 import org.example.observerpattern.subject.WeatherData;
 
@@ -12,6 +13,7 @@ public class WeatherStation {
     CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
     StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
     ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+    HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
     weatherData.setMeasurements(80, 65, 30.4f);
     System.out.println();
