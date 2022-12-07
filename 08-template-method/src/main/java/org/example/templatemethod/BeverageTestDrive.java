d79a411ff96ec6b67ae11d4dd8329b70c621e52f
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2022-12-07 18:20:47 +0700

    feat: DP-08: demo the template method pattern

--- 08-template-method/src/main/java/org/example/templatemethod/BeverageTestDrive.java
+++ 08-template-method/src/main/java/org/example/templatemethod/BeverageTestDrive.java
@@ -2,6 +2,13 @@ package org.example.templatemethod;
 
 public class BeverageTestDrive {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    CaffeineBeverage tea = new Tea();
+    CaffeineBeverage coffee = new Coffee();
+
+    System.out.println("\nMaking tea...");
+    tea.prepareRecipe();
+
+    System.out.println("\nMaking coffee...");
+    coffee.prepareRecipe();
   }
 }
