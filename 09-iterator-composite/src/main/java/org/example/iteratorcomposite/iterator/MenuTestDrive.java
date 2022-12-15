9cf9f31ad9d86a20b5f6e30b1ca18193051e59dd
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2022-12-15 23:37:19 +0700

    feat: DP-09: add the cafe menu

--- 09-iterator-composite/src/main/java/org/example/iteratorcomposite/iterator/MenuTestDrive.java
+++ 09-iterator-composite/src/main/java/org/example/iteratorcomposite/iterator/MenuTestDrive.java
@@ -4,8 +4,9 @@ public class MenuTestDrive {
   public static void main(String[] args) {
     PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
     DinerMenu dinerMenu = new DinerMenu();
+    CafeMenu cafeMenu = new CafeMenu();
 
-    Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+    Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
 
     waitress.printMenu();
   }
--- 09-iterator-composite/src/main/java/org/example/iteratorcomposite/iterator/Waitress.java
+++ 09-iterator-composite/src/main/java/org/example/iteratorcomposite/iterator/Waitress.java
@@ -7,15 +7,19 @@ import lombok.RequiredArgsConstructor;
 public class Waitress {
   private final Menu pancakeHouseMenu;
   private final Menu dinerMenu;
+  private final Menu cafeMenu;
 
   public void printMenu() {
     Iterator<MenuItem> pancakeHouseIterator = pancakeHouseMenu.createIterator();
     Iterator<MenuItem> dinerMenuIterator = dinerMenu.createIterator();
+    Iterator<MenuItem> cafeMenuIterator = cafeMenu.createIterator();
 
     System.out.println("MENU\n----\nBREAKFAST");
     printMenu(pancakeHouseIterator);
     System.out.println("\nLUNCH");
     printMenu(dinerMenuIterator);
+    System.out.println("\nDINNER");
+    printMenu(cafeMenuIterator);
   }
 
   public void printMenu(Iterator<MenuItem> iterator) {
